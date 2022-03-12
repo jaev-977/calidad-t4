03dc1eae73a3358ab45be1546c0fe450415b085f
jaev-977 <john-977@hotmail.com>
2022-03-12 00:07:20 -0500

    final2

--- ejer8.py
+++ ejer8.py
@@ -4,15 +4,15 @@ import unittest
 class TestMax(unittest.TestCase):
 
     def testMax_success(self):
-        resultado = max(1, 2)
-        self.assertEqual(resultado, 2)
+        resultado = max([4, 9])
+        self.assertEqual(resultado, 9)
     
     def testMax_assertIsInstance(self):
-        resultado = max(1, 2)
+        resultado = max([4, 9])
         self.assertIsInstance(resultado, int)
     
     def testMax_assertNotIsInstance(self):
-        resultado = max(1, 2)
+        resultado = max([4, 9])
         self.assertNotIsInstance(resultado, str)
 
 
--- ejer9.py
+++ ejer9.py
@@ -1,11 +1,12 @@
 from ejercicio import even_numbers
 import unittest
 
-class TestEvenNumbers(unittest.TestCase):
+class TestEvennum(unittest.TestCase):
 
-    def TestEvenNumbers_success(self):
+    def testEvennum_success(self):
         resultado = even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         self.assertEqual(resultado, [2, 4, 6, 8, 10])
 
+        
 if __name__ == '__main__':
     unittest.main()
