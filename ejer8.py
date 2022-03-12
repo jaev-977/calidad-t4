7be881f1ea678a3779685abe9d72aac41ee3bee4
John Alejandro Esteban Velandia <82247634+jaev-977@users.noreply.github.com>
2022-03-11 23:24:58 -0500

    Update ejer8.py

--- ejer8.py
+++ ejer8.py
@@ -4,15 +4,15 @@ import unittest
 class TestMax(unittest.TestCase):
 
     def testMax_success(self):
-        resultado = max(4, 9)
-        self.assertEqual(resultado, 9)
+        resultado = max(1, 2)
+        self.assertEqual(resultado, 2)
     
     def testMax_assertIsInstance(self):
-        resultado = max(4, 9)
+        resultado = max(1, 2)
         self.assertIsInstance(resultado, int)
     
     def testMax_assertNotIsInstance(self):
-        resultado = max(4, 9)
+        resultado = max(1, 2)
         self.assertNotIsInstance(resultado, str)
 
 
